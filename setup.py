d6158ec297b887d537ccbbc1a47437ed12861713
Mayankpratapsingh022 <mayankpratapsingh022@gmail.com>
2024-12-08 02:02:15 +0530

    pushed code to database

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mayank",
+    author_email="Mayankpratapsingh022@gmail.com",
+    packages=find_packages()
+
+
+)
